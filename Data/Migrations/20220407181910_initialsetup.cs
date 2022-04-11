e7c23aeafa66b4d7c45fbdb05603858244996bd0
Nizra-Thajudeen <fnizra09@gmail.com>
2022-04-11 13:15:00 +0530

    Completed CRUD operations

---
+++ Data/Migrations/20220407181910_initialsetup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookWebApp.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BookCategory = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace BookWebApp.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string BookName { get; set; }
+        public string BookCategory { get; set; }
+        public string Description { get; set; }
+        //public IFormFile BookImage { get; set; }
+        public Book()
+        {
+
+        }
+    }
+}
